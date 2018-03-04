1be951b11d4d9a28b33f8cf51c437433ab6326da
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-04 14:24:13 +0800

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 $("body").overhang({
   type: "success",
-  message: "Tumblr available? Try Tumblr version",
-  duration: 8,
+  message: "Tumblr available? Try var a = document.createElement("A");a.href = "https://xiaotan828.tumblr.com";a.target = "_blank";a.innerHTML = "Tumblr version";document.body.appendChild(a);",
+  
+  duration: 10,
   closeConfirm: true
 });
 });
