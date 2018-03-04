31097da887b55a0f78806d3084e01134c62b39a7
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-04 14:32:35 +0800

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $("body").overhang({
   type: "success",
-  message: "Tumblr available? Try window.location.href="https://xiaotan828.tubmlr.com";",
+  message: "Tumblr available? Try "xiaotan828.tubmlr.com",
   
   duration: 10,
   closeConfirm: true
