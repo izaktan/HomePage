1bf85c8a6d0f6b53bd8b1c0aeff47e192badd967
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-03 23:28:24 -0600

    Add files via upload

---
+++ js/notification.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$("body").overhang({
+  type: "error",
+  message: "Tumblr available? Go to <a href="https://xiaotan828.tumblr.com/">Tumblr Version</a>!",
+  duration: 5,
+  closeConfirm: true
+});
+});
