54de8d87ad43b1a50feca17897de35f54080fd92
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-04 13:47:08 +0800

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $("body").overhang({
   type: "error",
-  message: "Tumblr available? Go to <a href="https://xiaotan828.tumblr.com/">Tumblr Version</a>!",
+  message: "Tumblr available?",
   duration: 5,
   closeConfirm: true
 });
