75f7137b1e5d2452093d5471a1ec0b69c264124d
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-04 20:23:33 +0800

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 $("body").overhang({
   type: "success",
-  message: "Tumblr available? Try xiaotan828.tubmlr.com",
+  message: "Tumblr available? Try https://tumblr.txshore.tech/",
   duration: 10,
   closeConfirm: true
 });
