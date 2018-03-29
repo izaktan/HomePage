d485de9038132a6c53f580021f99a2565dc2c2cd
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-29 19:23:50 +0800

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -4,7 +4,8 @@ $("body").overhang({
    textColor: "#FFFAFA",
    primary: "#222222",
    accent: "#555555",
-   message: "Across the GFW we can reach every corner in the world.",
+   html: true,
+   message: "Across the Great Wall<br>we can reach every corner in the world.",
   duration: 10,
   closeConfirm: true
 });
