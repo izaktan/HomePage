74b746ccb8555c66512a812ccdf4ebcf53c688da
xiaotan828 <33599274+xiaotan828@users.noreply.github.com>
2018-03-29 19:03:52 +0800

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 $("body").overhang({
-  custom: true,
-  textColor: "#FFFAFA",
-  primary: "#222222",
-  accent: "#555555",
-  message: "Across the Great Wall we can reach every corner in the world."
-  duration: 10,
+   custom: true,
+   textColor: "#FFFAFA",
+   primary: "#222222",
+   accent: "#555555",
+   message: "Across the Great Wall we can reach every corner in the world."
   closeConfirm: true
 });
 });
