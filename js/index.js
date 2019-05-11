27ed03ba75e2569c763b18ca3c769c9678cd61b0
xiaotan828 <xiaotan828@gmail.com>
2019-05-11 16:45:03 +0800

    change

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 (function() {
   $('.menu-trigger').on('click', function(e) {
+    $(this).toggleClass('ripple');
     e.preventDefault();
     return $('#circle-menu').toggleClass('open');
   });
-
 }).call(this);
